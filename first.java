7ecea165f786637f7d9f475d4b3fb5054706dccf
ankitanmol26 <ankitanmol26@gmail.com>
2026-06-23 22:48:50 +0530

    Update Java files

--- first.java
+++ first.java
@@ -409,3 +409,58 @@
 //                 System.out.println("Total factors: " + factors);
 //         }
 // }
+
+//pattern problems
+// public class first{
+//     public static void main(String[] args){
+//       int n = 5;
+//       for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= i; j++){
+            
+//         System.out.print("* ");
+//       }  
+//       System.out.println();
+//     }
+// }
+// }
+
+// public class first{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i = n; i >= 1; i--){
+//             for(int j = i; j >= 1; j--){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class first{
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= i; j++) {
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+public class first {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
