4eafc3e091d15ee316a1d463c053ce590a9370ac
ankitanmol26 <ankitanmol26@gmail.com>
2026-06-26 22:32:09 +0530

    Save Java exercise file

--- first.java
+++ first.java
@@ -450,17 +450,17 @@
 //     }
 // }
 
-public class first {
-    public static void main(String[] args) {
-        int n = 5;
+// public class first {
+//     public static void main(String[] args) {
+//         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
+//         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(j);
+//             }
 
-            System.out.println();
-        }
-    }
-}
+//             System.out.println();
+//         }
+//     }
+// }
